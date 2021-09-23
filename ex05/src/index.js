82c6a88cf8a4f7dbe0f839bcba271776fc66f078
jasminHadzialic <hadzialic.jasmin@gmail.com>
2021-09-23 20:57:52 +0200

    nest one array with another

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+var myNestedArray = [["Toblerone", 5]];
+function myNestedFunction(arr) {
+    myNestedArray = [...arr];
+    myNestedArray.push(["Milka", 3]);
+    return myNestedArray;
+}
+// Only change code above this line
+
+console.log(myNestedFunction(myNestedArray)); // Change this line
+module.exports = myNestedFunction;
