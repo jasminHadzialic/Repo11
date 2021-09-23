ecfedf547003ee29d9a304e62c75163dd3af381f
jasminHadzialic <hadzialic.jasmin@gmail.com>
2021-09-23 21:02:58 +0200

    return the largest number

---
+++ ex07/src/index.js
@@ -0,0 +1,19 @@
+// Only change code abelow this line
+function largestNumFromArr(arr) {
+    var maxNumArray = [];
+    for (var i = 0; i < arr.length; i += 1) {
+        var oneArray = arr[i];
+        var largestNumber = 0;
+        for ( var j = 0; j < oneArray.length; j += 1) {
+            if (largestNumber < oneArray[j]) {
+                largestNumber = oneArray[j];
+            }
+        }
+        maxNumArray.push(largestNumber);
+    }
+    return maxNumArray;
+}
+// Only change code above this line
+console.log(largestNumFromArr([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestNumFromArr([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));  // Change this line
+module.exports = largestNumFromArr;
